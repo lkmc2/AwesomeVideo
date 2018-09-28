8fb076ac79c82e0500eca6cb4167f34cd7ca546d
lkmc2 <lkmc2@163.com>
2018-09-28 17:27:48 +0800

    将Mapper名改成单数

---
+++ src/main/java/com/lin/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lin.dao;
+
+import com.lin.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
