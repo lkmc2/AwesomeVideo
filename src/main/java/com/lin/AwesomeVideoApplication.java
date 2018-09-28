8c15f214414a5045c5075222a67c996095d4b217
lkmc2 <lkmc2@163.com>
2018-09-28 16:12:23 +0800

    初始化项目

---
+++ src/main/java/com/lin/AwesomeVideoApplication.java
@@ -0,0 +1,12 @@
+package com.lin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwesomeVideoApplication.class, args);
+    }
+}
